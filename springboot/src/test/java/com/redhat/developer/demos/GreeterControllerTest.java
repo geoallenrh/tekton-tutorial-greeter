906e4da3f54bf4f182dc7b1161ec5105052a2044
geoallenrh <geoffallen@redhat.com>
2021-03-09 06:47:14 -0600

    test

--- springboot/src/test/java/com/redhat/developer/demos/GreeterControllerTest.java
+++ springboot/src/test/java/com/redhat/developer/demos/GreeterControllerTest.java
@@ -24,6 +24,6 @@ public class GreeterControllerTest {
   @Test
   public void greetingShouldReturnDefaultMessage() throws Exception {
     assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/", String.class))
-        .contains("It's a Live Action Demo from Tekton 😺🚀");
+        .contains("It's a Live Action Demo2 from Tekton 😺🚀");
   }
 }
