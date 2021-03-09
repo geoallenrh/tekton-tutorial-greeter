1cf9e86e8c014fd699b1006dfe2e1dcea098df25
geoallenrh <geoffallen@redhat.com>
2021-03-09 06:51:20 -0600

    test

--- springboot/src/main/java/com/redhat/developer/demos/GreeterController.java
+++ springboot/src/main/java/com/redhat/developer/demos/GreeterController.java
@@ -8,6 +8,6 @@ public class GreeterController {
 
   @RequestMapping("/")
   public String hello() {
-    return "It's a Live Action Demo from Tekton 😺🚀";
+    return "It's a Live Action Demo2 from Tekton 😺🚀";
   }
 }
